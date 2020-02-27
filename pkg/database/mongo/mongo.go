d21d8bbd22e924dce632ca1b52ced3c6e23236fd
yitume-saber <yitume@163.com>
2020-02-27 21:59:56 +0800

    新增mongo

--- pkg/database/mongo/mongo.go
+++ pkg/database/mongo/mongo.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"sync"
-
 	"github.com/BurntSushi/toml"
 	"github.com/globalsign/mgo"
 	_ "github.com/go-sql-driver/mysql"
@@ -25,13 +24,29 @@ func Register() common.Caller {
 	return defaultCaller
 }
 
-func Caller(name string) *mgo.Database {
+func Caller(name string) *mgo.Session {
 	obj, ok := defaultCaller.caller.Load(name)
 	if !ok {
 		return nil
 	}
-	return obj.(*mgo.Database)
+	return obj.(*mgo.Session)
 }
+
+
+func CopyDb(name string) *mgo.Database {
+	obj, ok := defaultCaller.caller.Load(name)
+	if !ok {
+		return nil
+	}
+	session := obj.(*mgo.Session)
+	for cfgName, cfg := range defaultCaller.cfg.Muses.Mongo {
+		if cfgName == name {
+			return  session.Copy().DB(cfg.Database)
+		}
+	}
+	return nil
+}
+
 
 func (c *callerStore) InitCfg(cfg []byte) error {
 	if err := toml.Unmarshal(cfg, &c.cfg); err != nil {
@@ -51,15 +66,24 @@ func (c *callerStore) InitCaller() error {
 	return nil
 }
 
-func provider(cfg CallerCfg) (resp *mgo.Database, err error) {
+func provider(cfg CallerCfg) (resp *mgo.Session, err error) {
 	session, err := mgo.Dial(cfg.URL)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
 	}
+	mgo.SetLogger(&cLogger{})
 	mgo.SetDebug(cfg.Debug)
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
-	return session.DB(cfg.Database), err
+	return session, err
+}
+
+
+type cLogger struct {}
+
+func (c *cLogger) Output(calldepth int, s string) error {
+	fmt.Println("calldepth: ",calldepth,", s: ",s)
+	return nil
 }
